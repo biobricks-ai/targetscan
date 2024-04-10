f7cf0a6453df4bfad73d7cacd323aa58e3420252
bhlieberman <bhlieberman93@gmail.com>
2024-04-10 15:37:17 +0000

    formatting changes for readability

--- R/download.R
+++ R/download.R
@@ -6,7 +6,9 @@ library(fs)
 options(timeout=1800) # download timeout
 
 outs <- fs::dir_create("cache/download") # Create directory to which the downloaded files will be written 
-href <- rvest::read_html("https://www.targetscan.org/cgi-bin/targetscan/data_download.vert80.cgi") |> html_elements("a") |> html_attr("href") 
+href <- rvest::read_html("https://www.targetscan.org/cgi-bin/targetscan/data_download.vert80.cgi") |>
+  rvest::html_elements("a") |>
+  rvest::html_attr("href")
 dataref <- keep(href,~ grepl("*.zip$",.)) |>  map_chr(~ paste0("https://www.targetscan.org",.)) # URLs of files to be downloaded
 files   <- fs::path_file(dataref) |> partial(fs::path,outs)() # output paths
 
