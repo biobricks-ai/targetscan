51089e97c12927ebf64bacb66639b70869b8918e
bhlieberman <bhlieberman93@gmail.com>
2024-04-15 17:33:49 +0000

    skips downloading the perl archives and removes the cleanup script

--- stages/01_download.R
+++ stages/01_download.R
@@ -9,7 +9,9 @@ outs <- fs::dir_create("cache/download") # Create directory to which the downloa
 href <- rvest::read_html("https://www.targetscan.org/cgi-bin/targetscan/data_download.vert80.cgi") |>
   rvest::html_elements("a") |>
   rvest::html_attr("href")
-dataref <- keep(href,~ grepl("*.zip$",.)) |>  map_chr(~ paste0("https://www.targetscan.org",.)) # URLs of files to be downloaded
+dataref <- keep(href,~ grepl("*.zip$",.)) |>
+  discard(~ grepl(r"((targetscan_70\.zip|targetscan_70_BL_PCT\.zip|TargetScan7_context_scores\.zip)$)", .)) |>
+  map_chr(~ paste0("https://www.targetscan.org",.)) # URLs of files to be downloaded
 files   <- fs::path_file(dataref) |> partial(fs::path,outs)() # output paths
 
 fs::dir_ls("cache/download") |> fs::file_delete() # delete any files already present 
